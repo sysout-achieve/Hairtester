2402a5ba24a62203ba7873aa8368e0237485fcd5
sysout-achieve <35280158+sysout-achieve@users.noreply.github.com>
2018-06-08 15:10:48 +0900

    Update payment

--- Connect/app/src/main/java/com/example/msi/connect/KakaoActivity.java
+++ Connect/app/src/main/java/com/example/msi/connect/KakaoActivity.java
@@ -2,6 +2,8 @@ package com.example.msi.connect;
 
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -9,11 +11,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-
-import com.example.msi.connect.KakaoWebViewClient;
+import android.webkit.WebViewClient;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,20 +34,22 @@ import cz.msebera.android.httpclient.util.EntityUtils;
 
 public class KakaoActivity extends Activity {
 
+    public Context mContext;
     private WebView mainWebView;
     private final String APP_SCHEME = "iamportkakao://";
-//    String userID;
+    //    String userID;
+    WebViewInterface mWebViewInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kakao);
-
+        mContext = this.getApplicationContext();
         Intent intent = getIntent();
 
         String userID = intent.getStringExtra("userID");
         String salename = intent.getStringExtra("salename");
-        int saleprice = intent.getIntExtra("saleprice",10000);
+        int saleprice = intent.getIntExtra("saleprice", 10000);
         String staffid = intent.getStringExtra("staffid");
         String phone = intent.getStringExtra("phone");
         mainWebView = (WebView) findViewById(R.id.mainWebView);
@@ -53,14 +57,22 @@ public class KakaoActivity extends Activity {
         WebSettings settings = mainWebView.getSettings();
         settings.setJavaScriptEnabled(true);
 
-        mainWebView.loadUrl("http://13.125.234.222/new/payment.php?userid="+userID+"&salename="+salename+"&saleprice="+saleprice+"&staffid="+staffid+"&phone="+phone);
+        mainWebView.loadUrl("http://13.125.234.222/new/payment.php?userid=" + userID + "&salename=" + salename + "&saleprice=" + saleprice + "&staffid=" + staffid + "&phone=" + phone);
+
+        mWebViewInterface = new WebViewInterface(KakaoActivity.this, mainWebView);
+        mainWebView.addJavascriptInterface(mWebViewInterface, "android");
     }
 
 
     @Override
     protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-        setIntent(intent);
+        String url = intent.toString();
+
+        if (url.startsWith(APP_SCHEME)) {
+            // "iamportapp://https://pgcompany.com/foo/bar"와 같은 형태로 들어옴
+            String redirectURL = url.substring(APP_SCHEME.length() + "://".length());
+            mainWebView.loadUrl(redirectURL);
+        }
     }
 
     @Override
@@ -68,24 +80,72 @@ public class KakaoActivity extends Activity {
         super.onResume();
 
         Intent intent = getIntent();
-        if ( intent != null ) {
+        if (intent != null) {
             Uri intentData = intent.getData();
 
-            if ( intentData != null ) {
+            if (intentData != null) {
                 //카카오페이 인증 후 복귀했을 때 결제 후속조치
                 String url = intentData.toString();
 
-                if ( url.startsWith(APP_SCHEME) ) {
+                if (url.startsWith(APP_SCHEME)) {
                     String path = url.substring(APP_SCHEME.length());
-                    if ( "process".equalsIgnoreCase(path) ) {
+                    if ("process".equalsIgnoreCase(path)) {
                         mainWebView.loadUrl("javascript:IMP.communicate({result:'process'})");
                     } else {
+//                        mainWebView.loadUrl("app://application");
                         mainWebView.loadUrl("javascript:IMP.communicate({result:'cancel'})");
                     }
                 }
             }
         }
-
     }//onResume fin.
 
+    public class KakaoWebViewClient extends WebViewClient {
+        private Activity activity;
+
+        public KakaoWebViewClient(Activity activity) {
+            this.activity = activity;
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+            if (!url.startsWith("http://") && !url.startsWith("https://") && !url.startsWith("javascript:")) {
+                Intent intent = null;
+                try {
+                    intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME); //IntentURI처리
+                    Uri uri = Uri.parse(intent.getDataString());
+                    activity.startActivity(new Intent(Intent.ACTION_VIEW, uri));
+                    return true;
+                } catch (URISyntaxException ex) {
+                    return false;
+                } catch (ActivityNotFoundException e) {
+                    if (intent == null) return false;
+
+                    String packageName = intent.getPackage(); //packageName should be com.kakao.talk
+                    if (packageName != null) {
+                        activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + packageName)));
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            if (url.startsWith("app://")) {
+                Intent intent1 = new Intent(KakaoActivity.this, Main2Activity.class);
+                startActivity(intent1);
+                finish();
+            } else if (url.startsWith("cancel://")) {
+                Intent intent1 = new Intent(KakaoActivity.this, Main2Activity.class);
+                startActivity(intent1);
+                finish();
+            }
+            return false;
+        }
+
+        // 실패 salename=%EC%B
+        @Override
+        public void onPageFinished(WebView view, String url) {
+
+        }
+    }
 }
